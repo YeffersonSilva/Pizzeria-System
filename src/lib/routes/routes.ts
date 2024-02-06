51e59154fb6a3d188f785ad7a2276366f905a641
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-02-06 14:16:05 -0300

    estrotura inicial

---
+++ src/lib/routes/routes.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/teste", (req: Request, res: Response) => {
+    throw new Error("Error message test");
+})
+
+
+export { router };
+
+
+
